f59b49ddfb9c0b55ea485e9b2bb0da4123857f34
Mar Sob <irenicus30@gmail.com>
2019-12-18 19:47:27 +0100

    add second example

--- NN2.py
+++ NN2.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def nonlin(x, deriv=False):
+def lin(x, deriv=False):
   if(deriv==True):
-     return (x*(1-x))
-  return 1/(1+np.exp(-x))
+     return np.ones(x.shape)
+  return x
 
 X = np.array([
       [0,0],
@@ -24,20 +24,30 @@ np.random.seed(1)
 syn0 = 2*np.random.random((2,4)) - 1
 syn1 = 2*np.random.random((4,1)) - 1
 
+
+print("This are weights syn0 when the training is finished")
+print("syn0")
+print(syn0)
+
+print("This are weights syn1 when the training is finished")
+print("syn1")
+print(syn1)
+
+
 #training step
 for j in range(100000):  
     
     l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
-    
+    l1 = lin(np.dot(l0, syn0))
+    l2 = lin(np.dot(l1, syn1))
+    # print(l1) 
     l2_error = y - l2
     if(j % 10000) == 0: 
         print("Error: " + str(np.mean(np.abs(l2_error))))
         
-    l2_delta = l2_error*nonlin(l2, deriv=True)
+    l2_delta = l2_error*lin(l2, deriv=True)
     l1_error = l2_delta.dot(syn1.T)
-    l1_delta = l1_error * nonlin(l1,deriv=True)
+    l1_delta = l1_error * lin(l1,deriv=True)
     
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
